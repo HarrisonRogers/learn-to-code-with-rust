04f8f87021536a5dd510ac57a8b387c81be68434
harrison rogers <harrison.rogers05@gmail.com>
2026-01-13 21:24:35 +0900

    practicing error handling

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,1 +1,35 @@
-fn main() {}
+// --- Panic
+// fn main() {
+//     let a = 1;
+//     panic!("Error: oh shit boi")
+// }
+
+// --- Process Module
+// use std::process;
+
+// fn main() {
+//     process::exit(1)
+// }
+
+// --- eprinln!()
+// fn main() {
+//     println!("Beans");
+
+//     // this will segregate the error message if not a failiure (A bit pointless tbh)
+//     eprintln!("Error Beans");
+// }
+
+// Writing to file
+use std::{fs::File, process};
+
+fn main() {
+    let file = match File::open("story.txt") {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("No file here bruh: {}", error);
+            process::exit(1);
+        }
+    };
+
+    println!("{:#?}", file)
+}
