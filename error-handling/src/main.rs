5e9da58b513f4556163fe2aa9aaaa2a01f9ac9ff
harrison rogers <harrison.rogers05@gmail.com>
2026-01-16 10:16:53 +0900

    ? operator

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -19,17 +19,89 @@
 //     eprintln!("Error Beans");
 // }
 
-// Writing to file
-use std::{fs::File, process};
+// Writing to file & asking user for input & Reading the files content
+// use std::io::{Read, stdin};
+// use std::{fs::File, process};
+
+// fn main() {
+//     println!("Please enter the name of the file you'd like to read");
+//     let mut input = String::new();
+
+//     let user_requested_file = stdin().read_line(&mut input);
+
+//     if let Err(error) = user_requested_file {
+//         eprintln!("Something went wrong collecting user input: {}", error);
+//         process::exit(1)
+//     }
+
+//     let mut file = match File::open(input.trim()) {
+//         Ok(file) => file,
+//         Err(error) => {
+//             eprintln!("No file here bruh: {}", error);
+//             process::exit(1);
+//         }
+//     };
+
+//     let mut content = String::new();
+//     let read_operation = file.read_to_string(&mut content);
+
+//     if let Err(error) = read_operation {
+//         eprintln!("Something went wrong reading the file: {}", error);
+//         process::exit(1)
+//     }
+
+//     println!("{}", content);
+// }
+
+// --- Delegating errors & ? operator
+use std::fs::File;
+use std::io::{self, Read, stdin};
 
 fn main() {
-    let file = match File::open("story.txt") {
-        Ok(file) => file,
-        Err(error) => {
-            eprintln!("No file here bruh: {}", error);
-            process::exit(1);
-        }
-    };
+    let file_result = read_file();
 
-    println!("{:#?}", file)
+    match file_result {
+        Ok(contents) => println!("{}", contents),
+        Err(error) => {
+            eprintln!("There was an error: {}", error);
+        }
+    }
+}
+
+// fn read_file() -> Result<String, io::Error> {
+//     println!("Please enter the name of the file you'd like to read");
+//     let mut input = String::new();
+
+//     let user_requested_file = stdin().read_line(&mut input);
+
+//     if let Err(error) = user_requested_file {
+//         return Err(error);
+//     }
+
+//     let mut file = match File::open(input.trim()) {
+//         Ok(file) => file,
+//         Err(error) => return Err(error),
+//     };
+
+//     let mut content = String::new();
+//     let read_operation = file.read_to_string(&mut content);
+
+//     if let Err(error) = read_operation {
+//         return Err(error);
+//     }
+
+//     Ok(content)
+// }
+
+// --- ? operator
+fn read_file() -> Result<String, io::Error> {
+    println!("Please enter the name of the file you'd like to read");
+
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
+
+    let mut content = String::new();
+    File::open(input.trim())?.read_to_string(&mut content)?;
+
+    Ok(content)
 }
